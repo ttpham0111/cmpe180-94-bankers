0071267112f64dfe96bd16a47b6494c04387d836
Bob729 <bohan612@gmail.com>
2016-11-14 20:23:49 -0800

    Update bank.cpp

--- src/bank.cpp
+++ src/bank.cpp
@@ -17,3 +17,47 @@ int Bank::request_resources(int customer_id, int num_resources, int resources[])
 int Bank::release_resources(int customer_id, int num_resources, int resources[]) {
   return 0;
 }
+
+int release_resources(int customer_id, int request[])
+{
+	for (int i = 0; i < NUMBER_OF_RESOURCES; ++i)
+	{
+		if (request[i] > need[customer_id][i])
+		{
+			/// raise error
+			exit(1);
+		}
+	}
+	
+	for (int i = 0; i < NUMBER_OF_RESOURCES; ++i)
+	{
+		if (request[i] > available[i])
+		{
+			////wait for the request
+		}
+	}
+	
+	for (int i = 0; i < NUMBER_OF_RESOURCES; ++i)
+	{
+		available[i] -= request[i];
+		allocation[customer_id][i] += request[i];
+		need[customer_id][i] -= request[i];
+	}
+	
+	bool isSafe = true;
+	if (isSafe)
+	{
+		return 0;
+	}
+	else
+	{
+		for (int i = 0; i < NUMBER_OF_RESOURCES; ++i)
+		{
+			available[i] += request[i];
+			allocation[customer_id][i] -= request[i];
+			need[customer_id][i] += request[i];
+		}
+		
+		return -1;
+	}
+}
